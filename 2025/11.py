fe389fe2ce088468973b265cafd0d699da92da0e
Ivan Kolombet <kolombet@ostrovok.ru>
2025-12-11 16:30:26 +0300

    2025 day 11

---
+++ 2025/11.py
@@ -0,0 +1,50 @@
+import functools
+
+import aoc
+import networkx as nx
+
+
+def silver(lines):
+    g = nx.DiGraph()
+    for line in lines:
+        src, dsts = line.split(":")
+        for dst in dsts.split():
+            dst = dst.strip()
+            g.add_edge(src, dst)
+
+    return len(list(nx.all_simple_paths(g, "you", "out")))
+
+
+def gold(lines):
+    g = nx.DiGraph()
+    nodes = set()
+    for line in lines:
+        src, dsts = line.split(":")
+        for dst in dsts.split():
+            dst = dst.strip()
+            g.add_edge(src, dst)
+            nodes.add(dst)
+            nodes.add(src)
+
+    res = solve(g, "svr", "out")
+    return res
+
+
+@functools.cache
+def solve(g, src, dst, has_dac=False, has_fft=False):
+    res = 0
+    for nb in g.successors(src):
+        if nb == dst and has_dac and has_fft:
+            res += 1
+        else:
+            res += solve(g, nb, dst, has_dac or nb == "dac", has_fft or nb == "fft")
+    return res
+
+
+def main():
+    print("silver:", silver(aoc.stdin()))
+    print("gold:", gold(aoc.stdin()))
+
+
+if __name__ == "__main__":
+    main()
